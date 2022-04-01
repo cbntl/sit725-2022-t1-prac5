50701c0dfcc1121ed0838de333a803f50b795acc
cbntl <claire.bantilan@gmail.com>
2022-04-01 16:31:32 +1100

    Final Commitnpm

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
+require('dotenv').config()
 var express = require("express")
 var app = express()
+var cors = require("cors")
+const MongoClient = require('mongodb').MongoClient;
+let projectCollection;
+
+//Database Connection
+const url = "mongodb+srv://admin:admin@cluster0.dbnau.mongodb.net/SIT725_2022_T1?retryWrites=true&w=majority"
+const client = new MongoClient(url,{ useNewUrlParser: true })
 
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const createCollection = (collectionName) => {
+    client.connect((err,db) => {
+        projectCollection = client.db().collection(collectionName);
+        if(!err) {
+            console.log('MongoDB Connected')
+        }
+        else {
+            console.log("DB Error: ", err);
+            process.exit(1);
+        }
+    })
+}
+
+const insertProjects = (project,callback) => {
+    projectCollection.insert(project,callback);
+}
+
+const getProjects = (callback) => {
+    projectCollection.find({}).toArray(callback);
+}
 
 const addNumbers = (number1, number2) => {
     var num1 = parseInt(number1)
@@ -11,6 +40,45 @@ const addNumbers = (number1, number2) => {
     var result = num1 + num2;
     return result;
 }
+
+const cardList = [
+    {
+        title: "Kitten 2",
+        image: "images/kitten-2.jpg",
+        link: "About Kitten 2",
+        description: "Demo desciption about kitten 2"
+    },
+    {
+        title: "Kitten 3",
+        image: "images/kitten-3.jpg",
+        link: "About Kitten 3",
+        description: "Demo desciption about kitten 3"
+    }
+]
+
+app.get('/api/projects',(req,res) => {
+    getProjects((err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+        }
+        else {
+            res.json({statusCode: 200, message:"Success", data: result})
+        }
+    })
+})
+
+app.post('/api/projects',(req,res) => {
+    console.log("New Project added", req.body)
+    var newProject = req.body;
+    insertProjects(newProject,(err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+        }
+        else {
+            res.json({statusCode: 200, message:"Project Successfully added", data: result})
+        }
+    })
+})
 
 app.get("/addTwoNumbers",(req,res) => {
     var number1 = req.query.number1;
@@ -23,4 +91,5 @@ var port = process.env.port || 3000;
 
 app.listen(port,()=>{
     console.log("Application running at http://localhost:"+port)
+    createCollection("pets")
 })
